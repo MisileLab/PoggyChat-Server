05e2b6c780e941a52642df2842dd395622108fff
MisileLab <misileminecord@gmail.com>
2021-12-18 00:54:22 +0900

    pog with server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use std::net::{
-    TcpListener,
-    TcpStream
+use tokio::{
+    net::TcpListener,
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader}
 };
 
 use std::env::{
     args
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let port = args().nth(1).expect("does not have required args: port");
     println!("Try run server on {}", port);
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port));
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).await.unwrap();
     // Get message from client -> Save message history in file -> Send message to other client
+}
+
+async fn message_system(listener: TcpListener) {
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (readsocket, mut writesocket) = socket.split();
+            let mut reader = BufReader::new(readsocket);
+            let mut line = String::new();
+            
+            loop {
+                let read_bytes = reader.read_line(&mut line).await.unwrap();
+                if read_bytes < 1 {
+                    break;
+                }
+                writesocket.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
 }
